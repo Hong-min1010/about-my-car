3f049eb23b8956cef49c84d79f5ca882a2df81b2
Hong-min1010 <qza7367@gmail.com>
2025-11-26 15:59:37 +0900

    Initialize the project 🚀

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-page class="row items-center justify-evenly">
+    <example-component
+      title="Example component"
+      active
+      :todos="todos"
+      :meta="meta"
+    ></example-component>
+  </q-page>
+</template>
+
+<template>
+  <q-page padding>
+    <h1>차량 소모품 교체 주기 화면 테스트</h1>
+  </q-page>
+</template>
+
+<script setup lang="ts"></script>
